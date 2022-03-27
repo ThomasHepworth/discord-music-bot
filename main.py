2d73ada387f4a07767dea7e34b3e77ef03210bcc
th368 <tom_hepworth@hotmail.co.uk>
2022-03-27 17:56:36 +0100

    allow main.py to use new music cog from heroku

--- main.py
+++ main.py
@@ -2,10 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-# import other functions
-# from annoy_jack import annoy_jack
-# import all of the cogs
-# from music_cog import music_cog
+# import our music cog
 from new_music_bot import Music
 
 bot = commands.Bot(command_prefix='#')
@@ -15,15 +12,10 @@ bot.remove_command('help')
 
 # register the class with the bot
 bot.add_cog(Music(bot))
-# bot.add_cog(annoy_jack(bot))
 
 # load in the token from within hiroku
-# token = os.environ['TOKEN'] # use this if you're using the TOKEN within heroku
-token = os.getenv('TOKEN') # use this if you're running from docker
-
-#start the bot with our token
-# with open("token.txt") as file:
-#     token = file.read()
+token = os.environ['TOKEN'] # use this if you're using the TOKEN within heroku
+# token = os.getenv('TOKEN') # use this if you're running from docker
 
 # power up our bot
 bot.run(token)
